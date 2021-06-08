91e1ba10af763ed2dded7ea14f1c026b48745867
chaosJS <lichao779@sina.com>
2021-06-08 17:11:05 +0800

    update login out

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,30 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 import './style.css';
-import { Button } from 'antd';
+import { Button, message } from 'antd';
 import axios, { AxiosResponse } from 'axios';
+// const Home: React.FC = () => {
+// 	// const [isLogin, setIsLogin] = useState<boolean>(false);
+// 	const history = useHistory();
+// 	useEffect(() => {
+// 		axios.get('/api/isLogin').then((res: AxiosResponse) => {
+// 			if (!res.data?.data) {
+// 				history.push('/login');
+// 			}
+// 		});
+// 	}, []);
+// 	return (
+// 		<div className="home-page">
+// 			<Button type="primary" block>
+// 				爬取数据
+// 			</Button>
+// 			<Button block>展示数据</Button>
+// 			<Button type="link" block>
+// 				退出
+// 			</Button>
+// 		</div>
+// 	);
+// };
+
+// export default Home;
+
 const Home: React.FC = () => {
-	const [isLogin, setIsLogin] = useState<boolean>(false);
+	const [isLogin, setIsLogin] = useState<boolean>(true);
 	useEffect(() => {
-		let isSubscribed = true;
 		axios.get('/api/isLogin').then((res: AxiosResponse) => {
-			if (res.data?.data) {
-				if (isSubscribed) {
-					setIsLogin(true);
-				}
+			if (!res.data?.data) {
+				setIsLogin(false);
 			}
 		});
-		return () => {
-			isSubscribed = false;
-		};
 	}, []);
+	const handleLogout = () => {
+		axios.get('/api/logout').then((res: AxiosResponse) => {
+			if (res.data?.data) {
+				setIsLogin(false);
+				message.success('退出成功');
+			}
+		});
+	};
 	return isLogin ? (
 		<div className="home-page">
 			<Button type="primary" block>
 				爬取数据
 			</Button>
 			<Button block>展示数据</Button>
-			<Button type="link" block>
+			<Button type="link" block onClick={handleLogout}>
 				退出
 			</Button>
 		</div>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, message } from 'antd';
+import axios, { AxiosResponse } from 'axios';
+import { useHistory } from 'react-router-dom';
+
+import qs from 'qs';
 import './style.css';
 const layout = {
 	labelCol: { span: 8 },
@@ -9,8 +13,29 @@ const tailLayout = {
 	wrapperCol: { offset: 8, span: 8 },
 };
 const Login: React.FC = () => {
+	const history = useHistory();
 	const onFinish = (values: any) => {
 		console.log('Success:', values);
+		// 提交密码给后台 post 发送表单
+		axios
+			.post(
+				'/api/login',
+				qs.stringify({
+					password: values.password,
+				}),
+				{
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded',
+					},
+				}
+			)
+			.then((res: AxiosResponse) => {
+				if (res.data?.data) {
+					history.push('/');
+				} else {
+					message.error(res.data?.errMsg);
+				}
+			});
 	};
 
 	const onFinishFailed = (errorInfo: any) => {
